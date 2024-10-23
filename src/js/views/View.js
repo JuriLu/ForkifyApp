dfa50f7f51a38b48277a1feca7e53c8ca738eb3e
j.lubonja <j.lubonja@teamsystem.com>
2024-10-23 17:58:07 +0200

    Documentation fix

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -6,7 +6,7 @@ export default class View {
   /**
    * Render the recieved object to the DOM
    * @param {Object | Object[]} data The data to be rendered (e.g recipe)
-   * @param {boo;ean} [render=true]  If false, create markup string instead of rendering to the DOM
+   * @param {boolean} [render=true]  If false, create markup string instead of rendering to the DOM
    * @returns {undefined | string} A markup string is returned if render=false
    * @this {Object} View instance
    * @author Jurgen Lubonja
