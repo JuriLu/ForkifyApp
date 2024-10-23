7c729a1171200d44c9ac3d853a83c89ea90ddb1f
j.lubonja <j.lubonja@teamsystem.com>
2024-10-23 17:57:10 +0200

    Add documentation with : https://jsdoc.app/

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -1,75 +1,84 @@
 import icons from 'url:../../img/icons.svg';
 
 export default class View {
-    _data;
+  _data;
 
-    render(data,render = true) {
-        if (!data || (Array.isArray(data) && data.length === 0))
-            return this.renderError()
+  /**
+   * Render the recieved object to the DOM
+   * @param {Object | Object[]} data The data to be rendered (e.g recipe)
+   * @param {boo;ean} [render=true]  If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is returned if render=false
+   * @this {Object} View instance
+   * @author Jurgen Lubonja
+   * @todo Finish Implementation
+   */
+  render(data, render = true) {
+    if (!data || (Array.isArray(data) && data.length === 0))
+      return this.renderError()
 
-        this._data = data;
-        const markup = this._generateMarkup();
+    this._data = data;
+    const markup = this._generateMarkup();
 
-        if(!render) return markup
+    if (!render) return markup
 
-        this._clear();
-        this._parentElement.insertAdjacentHTML('afterbegin', markup);
-    }
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
 
-    update(data) {
-        this._data = data;
-        const newMarkup = this._generateMarkup();
+  update(data) {
+    this._data = data;
+    const newMarkup = this._generateMarkup();
 
-        const newDOM = document.createRange().createContextualFragment(newMarkup);
-        const newElements = Array.from(newDOM.querySelectorAll('*'));
-        const curElements = Array.from(this._parentElement.querySelectorAll('*'));
+    const newDOM = document.createRange().createContextualFragment(newMarkup);
+    const newElements = Array.from(newDOM.querySelectorAll('*'));
+    const curElements = Array.from(this._parentElement.querySelectorAll('*'));
 
 
-        newElements.forEach((newEl, i) => {
-            const curEl = curElements[i];
+    newElements.forEach((newEl, i) => {
+      const curEl = curElements[i];
 
-            //* Updates changed TEXT
-            if (
-                !newEl.isEqualNode(curEl) &&
-                newEl.firstChild?.nodeValue.trim() !== ''   //? Elements that contain text directly
-            ) {
-                curEl.textContent = newEl.textContent;
-            }
+      //* Updates changed TEXT
+      if (
+        !newEl.isEqualNode(curEl) &&
+        newEl.firstChild?.nodeValue.trim() !== ''   //? Elements that contain text directly
+      ) {
+        curEl.textContent = newEl.textContent;
+      }
 
-            //* Update changed ATTRIBUTES
-            if (!newEl.isEqualNode(curEl)) {
-                Array.from(newEl.attributes).forEach(attr => curEl.setAttribute(attr.name, attr.value))
-            }
-        })
+      //* Update changed ATTRIBUTES
+      if (!newEl.isEqualNode(curEl)) {
+        Array.from(newEl.attributes).forEach(attr => curEl.setAttribute(attr.name, attr.value))
+      }
+    })
 
-        /*
-        ? What will happen in this method is to create newMarkup but not render it. Instead we will genereate the newMarkup
-        ? and compare THE NEW HTML with the CURRENT HTML, and only change text and attributes that actually have changed from the old version
-        ? to the new version
+    /*
+    ? What will happen in this method is to create newMarkup but not render it. Instead we will genereate the newMarkup
+    ? and compare THE NEW HTML with the CURRENT HTML, and only change text and attributes that actually have changed from the old version
+    ? to the new version
 
-        ? We also convert the newMarkup (which is just a string) into a DOM object that is living in memory. So we can easily compare
-        ? the DOMs.
-         */
-    }
+    ? We also convert the newMarkup (which is just a string) into a DOM object that is living in memory. So we can easily compare
+    ? the DOMs.
+     */
+  }
 
-    _clear() {
-        this._parentElement.innerHTML = ''
-    }
+  _clear() {
+    this._parentElement.innerHTML = ''
+  }
 
-    renderSpinner() {
-        const markup = `
+  renderSpinner() {
+    const markup = `
        <div class="spinner">
          <svg>
             <use href="${icons}#icon-loader"></use>
          </svg>
        </div>
        `;
-        this._parentElement.innerHTML = ''
-        this._parentElement.insertAdjacentHTML('afterbegin', markup)
-    }
+    this._parentElement.innerHTML = ''
+    this._parentElement.insertAdjacentHTML('afterbegin', markup)
+  }
 
-    renderError(message = this._errorMessage) {
-        const markup = `
+  renderError(message = this._errorMessage) {
+    const markup = `
           <div class="error">
             <div>
               <svg>
@@ -79,12 +88,12 @@ export default class View {
             <p>${message}</p>
           </div>
         `
-        this._clear();
-        this._parentElement.insertAdjacentHTML('afterbegin', markup)
-    }
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup)
+  }
 
-    renderMessage(message = this._message) {
-        const markup = `
+  renderMessage(message = this._message) {
+    const markup = `
           <div class="message">
             <div>
               <svg>
@@ -94,7 +103,7 @@ export default class View {
             <p>${message}</p>
           </div>
         `
-        this._clear();
-        this._parentElement.insertAdjacentHTML('afterbegin', markup)
-    }
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup)
+  }
 };
