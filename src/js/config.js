362019eceb7f34c07c640513da2afdb318ddbda1
j.lubonja <j.lubonja@teamsystem.com>
2024-10-24 11:15:44 +0200

    key updated

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`
-export const API_KEY = 'e18a82cf-9833-4420-a095-4686dd3fcb16'  //* Needs refreshing every hour
+export const API_KEY = '23c0d52e-50a4-48f3-a78a-aece2b9eedab'  //* Needs refreshing every hour
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const MODAL_CLOSE_SEC = 2.5;
